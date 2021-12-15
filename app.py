32bd3c606f11775d70fed2a87e3b41d60578ab44
vikrant <vikrant.negi74@gmail.com>
2021-12-15 21:02:05 +0530

    Adding SQL Lite

--- app.py
+++ app.py
@@ -1,8 +1,21 @@
 from flask import Flask, render_template, url_for, flash, redirect, request
 from forms import RegistrationForm, LoginForm, ContactForm
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "946dafe6e87006141e819258961642b9"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+
+    def __repr__(self):
+        return f"User('{self.name}', '{self.email}')"
+
 
 @app.route("/", methods=['GET','POST'])
 def home():
