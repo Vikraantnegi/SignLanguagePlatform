9b5d901f3ecd961434e7b1c6ad358a7352a82f9d
vikrant <vikrant.negi74@gmail.com>
2021-12-15 22:06:13 +0530

    User Registration

--- signlang/__init__.py
+++ signlang/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "946dafe6e87006141e819258961642b9"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from signlang import routes
--- signlang/routes.py
+++ signlang/routes.py
@@ -1,5 +1,5 @@
 from flask import render_template, url_for, flash, redirect, request
-from signlang import app
+from signlang import app, db, bcrypt
 from signlang.forms import RegistrationForm, LoginForm, ContactForm
 from signlang.models import User
 
@@ -14,6 +14,13 @@ def home():
 @app.route("/register")
 def register():
     form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed_pwd = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(name=form.name.data, email=form.email.data, password=hashed_pwd)
+        db.session.add(user)
+        db.session.commit()
+        flash(f'Your account has been created, Congratulations! Please login to proceed.', 'success')
+        return redirect(url_for('login'))
     return render_template('register.html', form=form)
 
 @app.route("/login")
