d5ef62644a080c77cf61c5a30824ba571c7994c3
vikrant <vikrant.negi74@gmail.com>
2021-12-16 08:15:45 +0530

    Integrating Model

--- signlang/__init__.py
+++ signlang/__init__.py
@@ -4,11 +4,13 @@ from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
 UPLOAD_FOLDER = 'static/uploads/'
+MODEL_FOLDER = 'models/'
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "946dafe6e87006141e819258961642b9"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MODEL_FOLDER'] = MODEL_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
--- signlang/routes.py
+++ signlang/routes.py
@@ -1,10 +1,65 @@
 import os
+from tensorflow import keras
+import tensorflow as tf
+from keras.models import load_model
+import cv2
+import numpy as np
+from scipy import stats
+import pandas as pd
 from flask import render_template, url_for, flash, redirect, request
 from signlang import app, db, bcrypt
 from signlang.forms import RegistrationForm, LoginForm, ContactForm, UploadForm
 from signlang.models import User
 from flask_login import login_user, current_user, logout_user, login_required
 from werkzeug.utils import secure_filename
+
+model = load_model("models/SignModel.h5")
+
+words=[
+    'book', 'drink', 'computer', 'before', 'chair', 'go', 'clothes',
+    'who', 'candy', 'cousin', 'deaf', 'fine', 'help', 'no', 'thin',
+    'walk', 'year', 'yes', 'all', 'black', 'cool', 'finish', 'hot',
+    'like', 'many', 'mother', 'now', 'orange', 'table', 'thanksgiving',
+    'what', 'woman', 'bed', 'blue', 'bowling', 'can', 'dog', 'family',
+    'fish', 'graduate', 'hat', 'hearing', 'kiss', 'language', 'later',
+    'man', 'shirt', 'study', 'tall', 'white', 'wrong', 'accident',
+    'apple', 'bird', 'change', 'color', 'corn', 'cow', 'dance', 'dark',
+    'doctor', 'eat', 'enjoy', 'forget', 'give', 'last', 'meet', 'pink',
+    'pizza', 'play', 'school', 'secretary', 'short', 'time', 'want',
+    'work', 'africa', 'basketball', 'birthday', 'brown', 'but',
+    'cheat', 'city', 'cook', 'decide', 'full', 'how', 'jacket',
+    'letter', 'medicine', 'need', 'paint', 'paper', 'pull', 'purple',
+    'right', 'same', 'son', 'tell', 'thursday', 'visit', 'wait',
+    'water', 'wife', 'yellow', 'backpack', 'bar', 'brother', 'cat',
+    'check', 'class', 'cry', 'different', 'door', 'green', 'hair',
+    'have', 'headache', 'inform', 'knife', 'laugh', 'learn', 'movie',
+    'rabbit', 'read', 'red', 'room', 'run', 'show', 'sick', 'snow',
+    'take', 'tea', 'teacher', 'week', 'why', 'with', 'write',
+    'yesterday', 'again', 'bad', 'ball', 'bathroom', 'blanket', 'buy',
+    'call', 'coffee', 'cold', 'college', 'copy', 'cute', 'daughter',
+    'example', 'far', 'first', 'friend', 'good', 'happy', 'home',
+    'know', 'late', 'leave', 'list', 'lose', 'name', 'old', 'person',
+    'police', 'problem', 'remember', 'share', 'soon', 'stay', 'sunday',
+    'test', 'tired', 'trade', 'travel', 'window', 'you', 'about',
+    'approve', 'arrive', 'balance', 'banana', 'beard', 'because',
+    'boy', 'business', 'careful', 'center', 'chat', 'children',
+    'christmas', 'clock', 'close', 'convince', 'country', 'crash',
+    'day', 'discuss', 'dress', 'drive', 'drop', 'fat', 'feel',
+    'football', 'future', 'game', 'girl', 'government', 'hear', 'here',
+    'hope', 'house', 'husband', 'interest', 'join', 'light', 'live',
+    'make', 'mean', 'more', 'most', 'music', 'new', 'none', 'office',
+    'order', 'pants', 'party', 'past', 'pencil', 'plan', 'please',
+    'practice', 'president', 'restaurant', 'ride', 'russia', 'salt',
+    'sandwich', 'sign', 'since', 'small', 'some', 'south', 'student',
+    'teach', 'theory', 'tomato', 'train', 'ugly', 'war', 'where',
+    'your', 'always', 'animal', 'argue', 'baby', 'back', 'bake',
+    'bath', 'behind', 'bring', 'catch', 'cereal', 'champion', 'cheese',
+    'cough', 'crazy', 'delay', 'delicious', 'disappear', 'divorce',
+    'draw', 'east', 'easy', 'egg', 'environment', 'father', 'fault',
+    'flower', 'friendly', 'glasses', 'halloween', 'hard', 'heart',
+    'hour', 'humble', 'hurry', 'improve', 'internet', 'jump', 'kill',
+    'law', 'match', 'meat', 'milk', 'money'
+]
 
 @app.route("/", methods=['GET','POST'])
 def home():
@@ -51,6 +106,34 @@ def logout():
     logout_user()
     return redirect(url_for('home'))
 
+#translate button
+
+def getWord( path ):
+    frames=[]
+    cap=cv2.VideoCapture(path)
+    while(cap.isOpened()):
+            ret, frame = cap.read()
+            if ret==True:
+                try:
+                    frame=cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                    frame = cv2.resize(frame, (96,128), interpolation = cv2.INTER_AREA)
+                    frames.append(frame.reshape(128,96,1))
+                    
+                except:
+                    break
+            else:
+                break
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+        
+    cap.release()
+    cv2.destroyAllWindows()
+    frames=np.array(frames)            
+    pred=model.predict(frames/255)
+    word=stats.mode(np.argmax(pred,axis=1))[0][0]
+    word=words[word]
+    return word
+
 @app.route("/product", methods=['GET', 'POST'])
 @login_required
 def product():
@@ -62,5 +145,6 @@ def product():
         file.save(os.path.join(app.config['UPLOAD_FOLDER']))
         print('Upload_Video: ', + filename)
         flash('Video successfully uploaded and displayed below')
+        result = getWord(os.path.join(app.config['UPLOAD_FOLDER'] + filename))
         return render_template('product.html', form=form, result=result)
     return render_template('product.html', form=form, result=result)
